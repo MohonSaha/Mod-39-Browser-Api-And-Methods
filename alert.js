a22581c652c2335a9ce94c8f36f4b628caa4439c
MohonSaha <practicewithmohon@gmail.com>
2022-06-07 15:13:29 +0600

    adding code

--- alert.js
+++ alert.js
@@ -3,3 +3,23 @@
 const maComing = () => {
     alert('ma is coming. alert all');
 }
+
+const askPicnic = () => {
+    const response = confirm('Are you Going to picnic?');
+    // console.log(response);
+    if(response == true){
+        alert("send me the fee bikash");
+    }
+    else{
+        console.log("Okay as your wish");
+    }
+}
+
+
+const askName = () => {
+   const name = prompt('What is your name?')
+   if(name){
+    console.log(name);
+   }
+   
+}
