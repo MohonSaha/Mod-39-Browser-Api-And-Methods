2d134bd6c05cf47920045c3738e83aa532eae5eb
MohonSaha <practicewithmohon@gmail.com>
2022-06-07 14:38:51 +0600

    adding code

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student  = {name: 'jolil', friend: 'borsa', job: 'business',
+            makeMovie: function(name){
+                console.log(name);
+            }
+};
+
+student.makeMovie('Din raat')
+document.getElementById('name');
